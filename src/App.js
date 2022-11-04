2e3bbc5497be9c3586420114c1e6d646542110d3
Gyaneshwari91 <109033558+Gyaneshwari91@users.noreply.github.com>
2022-11-04 19:37:11 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import BasicExample from './Navbar';
+import {Navbar , Nav , Container} from "react-bootstrap"
+import ColorSchemesExample from './Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ColorSchemesExample/>
+    </div>
+  );
+}
+
+export default App;
